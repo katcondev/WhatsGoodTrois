340d9eee3749b276f5abd4247f0d8a5b6d97904f
katcontrerasdev <kat.contreras@gmail.com>
2023-08-03 14:15:52 -0700

    updated nav

--- app/index.js
+++ app/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Text, View, TouchableOpacity, Button } from "react-native";
 import commonStyles from "./styles";
-import { useNavigation } from "@react-navigation/native"; // Import the useNavigation hook
+
 import LibRoster from "./libations"; // Import the LibRoster component
 
 export default function Home() {
-  const navigation = useNavigation(); // Get the navigation object
-
   const handleSayLess = () => {
     // Navigate to the LibRoster screen
     console.log("bet!");
